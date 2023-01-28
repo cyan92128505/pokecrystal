a08a27c60d30e05626ab3b6d0da353ee3cde6b02
Andrew <andrewcarlisle90@gmail.com>
2023-01-28 19:09:37 +0000

    update kanto water encounters
    update kanto water encounters

--- data/wild/kanto_water.asm
+++ data/wild/kanto_water.asm
@@ -4,170 +4,170 @@ KantoWaterWildMons:
 
 	def_water_wildmons TOHJO_FALLS
 	db 4 percent ; encounter rate
-	db 20, MAGIKARP
-	db 20, SLOWPOKE
-	db 20, POLIWAG
+	db 40, POLIWRATH
+	db 40, SLOWBRO
+	db 40, SLOWKING
 	end_water_wildmons
 
 	def_water_wildmons VERMILION_PORT
 	db 2 percent ; encounter rate
-	db 35, TENTACOOL
+	db 30, STARYU
 	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 40, TENTACRUEL
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_4
 	db 4 percent ; encounter rate
-	db 10, MAGIKARP
-	db 5, MAGIKARP
-	db 10, POLIWAG
+	db 40, TENTACRUEL
+	db 40, GYARADOS
+	db 45, BLASTOISE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_6
 	db 2 percent ; encounter rate
-	db 10, PSYDUCK
-	db 5, PSYDUCK
-	db 10, GOLDUCK
+	db 30, PSYDUCK
+	db 30, POLIWHIRL
+	db 40, GOLDUCK
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_9
 	db 4 percent ; encounter rate
-	db 15, MAGIKARP
-	db 10, MAGIKARP
-	db 15, POLIWAG
+	db 30, CHINCHOU
+	db 35, QUAGSIRE
+	db 40, LANTURN
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_10_NORTH
 	db 4 percent ; encounter rate
-	db 15, MAGIKARP
-	db 10, MAGIKARP
-	db 15, POLIWAG
+	db 30, CHINCHOU
+	db 35, QUAGSIRE
+	db 40, LANTURN
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_12
 	db 6 percent ; encounter rate
-	db 25, TENTACOOL
-	db 25, QUAGSIRE
-	db 25, TENTACRUEL
+	db 30, TENTACOOL
+	db 30, STARYU
+	db 40, STARMIE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_13
 	db 6 percent ; encounter rate
-	db 25, TENTACOOL
-	db 25, QUAGSIRE
-	db 25, TENTACRUEL
+	db 30, SHELLDER
+	db 30, POLIWHIRL
+	db 40, POLITOED
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_19
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 40, TENTACRUEL
+	db 40, BLASTOISE
+	db 50, GYARADOS
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_20
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 40, CLOYSTER
+	db 40, ARCTOVISH
+	db 50, LAPRAS
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_21
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 40, STARMIE
+	db 40, KINGDRA
+	db 50, MILOTIC
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_22
 	db 2 percent ; encounter rate
-	db 10, POLIWAG
-	db 5, POLIWAG
-	db 10, POLIWHIRL
+	db 5, MAGIKARP
+	db 5, FEEBAS
+	db 50, VAPOREON
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_24
 	db 4 percent ; encounter rate
-	db 10, MAGIKARP
-	db 5, MAGIKARP
-	db 10, POLIWAG
+	db 40, POLITOED
+	db 40, GYARADOS
+	db 40, STARMIE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_25
 	db 4 percent ; encounter rate
-	db 10, MAGIKARP
-	db 5, MAGIKARP
-	db 10, POLIWAG
+	db 40, POLITOED
+	db 40, GYARADOS
+	db 40, STARMIE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_26
 	db 6 percent ; encounter rate
-	db 30, TENTACOOL
-	db 25, TENTACOOL
-	db 30, TENTACRUEL
+	db 40, QUAGSIRE
+	db 40, LANTURN
+	db 40, STARMIE
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_27
 	db 6 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACRUEL
+	db 40, KINGDRA
+	db 40, MILOTIC
+	db 40, GYARADOS
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_28
 	db 2 percent ; encounter rate
-	db 40, POLIWAG
-	db 35, POLIWAG
-	db 40, POLIWHIRL
+	db 65, LANTURN
+	db 65, CLOYSTER
+	db 65, QUAGSIRE
 	end_water_wildmons
 
 	def_water_wildmons PALLET_TOWN
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 40, TENTACRUEL
+	db 40, BLASTOISE
+	db 50, GYARADOS
 	end_water_wildmons
 
 	def_water_wildmons VIRIDIAN_CITY
 	db 2 percent ; encounter rate
-	db 10, POLIWAG
-	db 5, POLIWAG
-	db 10, POLIWHIRL
+	db 5, POLIWHIRL
+	db 5, CHINCHOU
+	db 40, POLIWRATH
 	end_water_wildmons
 
 	def_water_wildmons CERULEAN_CITY
 	db 4 percent ; encounter rate
-	db 10, MAGIKARP
-	db 5, MAGIKARP
-	db 10, POLIWAG
+	db 40, LANTURN
+	db 40, GYARADOS
+	db 40, STARMIE
 	end_water_wildmons
 
 	def_water_wildmons VERMILION_CITY
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
+	db 30, STARYU
 	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 40, TENTACRUEL
 	end_water_wildmons
 
 	def_water_wildmons CELADON_CITY
 	db 2 percent ; encounter rate
-	db 20, TENTACOOL
-	db 15, TENTACOOL
-	db 30, TENTACRUEL
+	db 40, WEEZING
+	db 40, TENTACRUEL
+	db 40, VAPOREON
 	end_water_wildmons
 
 	def_water_wildmons FUCHSIA_CITY
 	db 2 percent ; encounter rate
-	db 20, MAGIKARP
-	db 15, MAGIKARP
-	db 10, MAGIKARP
+	db 40, QUAGSIRE
+	db 40, SLOWBRO
+	db 40, ARCTOVISH
 	end_water_wildmons
 
 	def_water_wildmons CINNABAR_ISLAND
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
+	db 40, CLOYSTER
+	db 40, ARCTOVISH
+	db 50, LAPRAS
 	end_water_wildmons
 
 	db -1 ; end
